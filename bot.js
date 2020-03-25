d576a96721fefcba594ed8f5eae7de5f05a157c3
Martin Perren <martinperren@gmail.com>
2020-03-25 02:09:32 -0300

    first commit

---
+++ bot.js
@@ -0,0 +1,57 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+const Client = require('node-rest-client').Client;
+const Util = require('discord.js');
+const music = require('discord.js-musicbot-addon');
+
+
+
+
+
+
+client.on("ready", () => {
+console.log("Bot iniciado");
+	
+	client.user.setActivity(process.env.GAME, { type: 'LISTENING' })
+	.then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+	.catch(console.error);
+
+});
+
+
+
+music.start(client, {
+youtubeKey: process.env.YOUTUBE;
+
+  <command>: {
+    enabled: false,                    // True/False statement.
+    alt: ["name1","name2","name3"],    // Array of alt names (aliases).
+    help: "Help text.",                // String of help text.
+    name: "play"                       // Name of the command.
+    usage: "{{!}}", // Usage text. {{prefix}} will insert the bots prefix.
+    exclude: false                     // Excludes the command from the help command.
+  }
+});
+
+
+
+//   COMANDOS   //   COMANDOS   //   COMANDOS   //   COMANDOS   //   COMANDOS   //   COMANDOS   //   COMANDOS   //   COMANDOS   //   COMANDOS   
+
+
+
+
+client.on("message", async message => {
+	const args = message.content.slice(1).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+	const argsM = message.content.split(' ');
+
+
+
+	if (message.content.includes("huevo")) {
+		message.react("537716624296378399");
+	}
+	
+	
+
+});
+client.login(process.env.BOT_TOKEN);
