c9e9cd693a450db254d75254130cbbe8c0b622e6
Martin Perren <martinperren@gmail.com>
2020-03-25 22:29:59 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,9 +21,7 @@ console.log("Bot iniciado");
 
 
 music.start(client, {
-youtubeKey: "process.env.YOUTUBE",
-botPrefix:"!",
-botAdmins: ["211602677858172930"]
+youtubeKey: "process.env.YOUTUBE"
  
 });
 
