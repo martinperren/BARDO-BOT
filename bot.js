7c31e64e7957279f8cde8f7f19467bb49a3e4b3c
Martin Perren <martinperren@gmail.com>
2020-04-11 02:45:15 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -545,7 +545,6 @@ async function showProgress(channel, game, gameMessage, gameOver) {
 }
 
 async function startGame(channel, gameType) {
-	console.log("startGame joined");
 	const players = await gatherPlayers(channel);
 	if (players.length == 0) {
 		channel.send("Otra vez será... nadie entró a jugar :(");
@@ -581,7 +580,6 @@ async function startGame(channel, gameType) {
 }
 
 async function runGame(channel, game, players) {
-	console.log("runGame joined");
 	const gameMessage = await showProgress(channel, game);
 	const filter = ((m) =>
 		players.find((p) => (p.id == m.author.id)));
@@ -639,8 +637,7 @@ client.on('message', async (msg) => {
 	if (!msg.author.bot && msg.content.startsWith(prefix) && msg.channel.type === "text") {
 
 		const args = msg.content.slice(prefix.length).trim().split(' ').filter(word => word.trim().length > 0);
-		console.log("args 0: "+args[0]);
-		console.log("args 1: "+args[1]);
+
 
 		if(args[0]=="cancel"){
 			runningGames.delete(msg.channel.guild);
