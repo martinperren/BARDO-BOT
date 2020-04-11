799952a58e7ffc0e0b383d6687c157b68c3d18a3
Martin Perren <martinperren@gmail.com>
2020-04-11 01:50:24 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -653,6 +653,7 @@ client.on('message', async (msg) => {
                             gameType = "custom";
                             break;
                         default:
+                        console.log("default case");
                             msg.channel.send("Podes elegir entre el modo de juego \"custom\" o \"random\". Usa !jugar modo");
                             return;
                     
