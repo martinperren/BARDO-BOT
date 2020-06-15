2d32ad8f7d50bd5f01084aea3dbceb86fd598d35
Martin Perren <martinperren@gmail.com>
2020-06-14 21:51:04 -0300

    fixed

--- bot.js
+++ bot.js
@@ -268,8 +268,8 @@ client.on("message", async message => {
 
 		message.channel.send(
 		"\nUSER: "+data.all.RANKED_FLEX_SR.summonerName+
-		"\nELO SOLO: "+data.all.RANKED_FLEX_SR.tier +" "+ data.all.RANKED_FLEX_SR.rank+ data.all.RANKED_FLEX_SR.leaguePoints + "PL"+
-		"\nELO FLEX: "+data.all.RANKED_SOLO_5x5.tier +" "+ data.all.RANKED_SOLO_5x5.rank+data.all.RANKED_SOLO_5x5.leaguePoints+" PL"
+		"\nELO SOLO: "+data.all.RANKED_FLEX_SR.tier +" "+ data.all.RANKED_FLEX_SR.rank+" "+ data.all.RANKED_FLEX_SR.leaguePoints + "PL"+
+		"\nELO FLEX: "+data.all.RANKED_SOLO_5x5.tier +" "+ data.all.RANKED_SOLO_5x5.rank+" "+data.all.RANKED_SOLO_5x5.leaguePoints+" PL"
 		
 		);
 
