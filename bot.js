42d85280ff997fdb304c0627d38df25a21ce3b41
Martin Perren <martinperren@gmail.com>
2020-06-17 00:41:49 -0300

    test embed

--- bot.js
+++ bot.js
@@ -318,17 +318,40 @@ client.on("message", async message => {
 	if (message.content.startsWith("!m")) {
 
 
-		let data;
-		for (i = 0; i < 10; i++) {
-
-			
+		const embed = new Discord.RichEmbed()
+		.setTitle("This is your title, it can hold 256 characters")
+		.setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+		/*
+		 * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+		 */
+		.setColor(0x00AE86)
+		.setDescription("This is the main body of text, it can hold 2048 characters.")
+		.setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+		.setImage("http://i.imgur.com/yVpymuV.png")
+		.setThumbnail("http://i.imgur.com/p2qNFag.png")
+		/*
+		 * Takes a Date object, defaults to current date.
+		 */
+		.setTimestamp()
+		.setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+		/*
+		 * Inline fields may not display as inline if the thumbnail and/or image is too big.
+		 */
+		.addField("Inline Field", "They can also be inline.", true)
+		/*
+		 * Blank field, useful to create some space.
+		 */
+		.addBlankField(true)
+		.addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+	   
 		
 
 
 
+		let data;
+		for (i = 0; i < 10; i++) {
 
-			
-			let args = message.content.substring(1).split(" ");
+		let args = message.content.substring(1).split(" ");
 			args.splice(0, 1);
 			const username = args.join(" ");
 
@@ -360,11 +383,14 @@ client.on("message", async message => {
 
 			}
 
+embed.addField(data.participants[i].summonerName+" "+getChampionName(data.participants[i].championId))
 
-
-			console.log(data.participants[i].summonerName+getChampionName(data.participants[i].championId));
+			console.log(data.participants[i].summonerName+" "+getChampionName(data.participants[i].championId));
 		}
 
+
+
+		message.channel.send({embed});
 
 		//console.log(playerName +" "+ response);
 
