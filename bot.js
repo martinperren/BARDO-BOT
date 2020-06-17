3ff83df2961f8bf314147b4fe7e67ee45d43fa36
Martin Perren <martinperren@gmail.com>
2020-06-17 01:14:58 -0300

    fixed

--- bot.js
+++ bot.js
@@ -37,9 +37,9 @@ client.on("ready", () => {
 function getChampionName(key) {
 
 	var Champions = {
-		
-		'1':'Annie','2':'Olaf','3':'Galio','4':'TwistedFate','5':'XinZhao','6':'Urgot','7':'Leblanc','8':'Vladimir','9':'Fiddlesticks','10':'Kayle','11':'MasterYi','12':'Alistar','13':'Ryze','14':'Sion','15':'Sivir','16':'Soraka','17':'Teemo','18':'Tristana','19':'Warwick','20':'Nunu','21':'MissFortune','22':'Ashe','23':'Tryndamere','24':'Jax','25':'Morgana','26':'Zilean','27':'Singed','28':'Evelynn','29':'Twitch','30':'Karthus','31':'Chogath','32':'Amumu','33':'Rammus','34':'Anivia','35':'Shaco','36':'DrMundo','37':'Sona','38':'Kassadin','39':'Irelia','40':'Janna','41':'Gangplank','42':'Corki','43':'Karma','44':'Taric','45':'Veigar','48':'Trundle','50':'Swain','51':'Caitlyn','53':'Blitzcrank','54':'Malphite','55':'Katarina','56':'Nocturne','57':'Maokai','58':'Renekton','59':'JarvanIV','60':'Elise','61':'Orianna','62':'MonkeyKing','63':'Brand','64':'LeeSin','67':'Vayne','68':'Rumble','69':'Cassiopeia','72':'Skarner','74':'Heimerdinger','75':'Nasus','76':'Nidalee','77':'Udyr','78':'Poppy','79':'Gragas','80':'Pantheon','81':'Ezreal','82':'Mordekaiser','83':'Yorick','84':'Akali','85':'Kennen','86':'Garen','89':'Leona','90':'Malzahar','91':'Talon','92':'Riven','96':'KogMaw','98':'Shen','99':'Lux','101':'Xerath','102':'Shyvana','103':'Ahri','104':'Graves','105':'Fizz','106':'Volibear','107':'Rengar','110':'Varus','111':'Nautilus','112':'Viktor','113':'Sejuani','114':'Fiora','115':'Ziggs','117':'Lulu','119':'Draven','120':'Hecarim','121':'Khazix','122':'Darius','126':'Jayce','127':'Lissandra','131':'Diana','133':'Quinn','134':'Syndra','136':'AurelionSol','143':'Zyra','150':'Gnar','154':'Zac','157':'Yasuo','161':'Velkoz','163':'Taliyah','164':'Camille','201':'Braum','202':'Jhin','203':'Kindred','222':'Jinx','223':'TahmKench','236':'Lucian','238':'Zed','240':'Kled','245':'Ekko','254':'Vi','266':'Aatrox','267':'Nami','268':'Azir','412':'Thresh','420':'Illaoi','421':'RekSai','427':'Ivern','429':'Kalista','432':'Bard','497':'Rakan','498':'Xayah','516':'ORNN','517':'SYLAS','518':'NEEKO','523':'APHELIOS','555':'PYKE','235':'SENNA','875':'SETT'
-		   }
+
+		'1': 'Annie', '2': 'Olaf', '3': 'Galio', '4': 'TwistedFate', '5': 'XinZhao', '6': 'Urgot', '7': 'Leblanc', '8': 'Vladimir', '9': 'Fiddlesticks', '10': 'Kayle', '11': 'MasterYi', '12': 'Alistar', '13': 'Ryze', '14': 'Sion', '15': 'Sivir', '16': 'Soraka', '17': 'Teemo', '18': 'Tristana', '19': 'Warwick', '20': 'Nunu', '21': 'MissFortune', '22': 'Ashe', '23': 'Tryndamere', '24': 'Jax', '25': 'Morgana', '26': 'Zilean', '27': 'Singed', '28': 'Evelynn', '29': 'Twitch', '30': 'Karthus', '31': 'Chogath', '32': 'Amumu', '33': 'Rammus', '34': 'Anivia', '35': 'Shaco', '36': 'DrMundo', '37': 'Sona', '38': 'Kassadin', '39': 'Irelia', '40': 'Janna', '41': 'Gangplank', '42': 'Corki', '43': 'Karma', '44': 'Taric', '45': 'Veigar', '48': 'Trundle', '50': 'Swain', '51': 'Caitlyn', '53': 'Blitzcrank', '54': 'Malphite', '55': 'Katarina', '56': 'Nocturne', '57': 'Maokai', '58': 'Renekton', '59': 'JarvanIV', '60': 'Elise', '61': 'Orianna', '62': 'MonkeyKing', '63': 'Brand', '64': 'LeeSin', '67': 'Vayne', '68': 'Rumble', '69': 'Cassiopeia', '72': 'Skarner', '74': 'Heimerdinger', '75': 'Nasus', '76': 'Nidalee', '77': 'Udyr', '78': 'Poppy', '79': 'Gragas', '80': 'Pantheon', '81': 'Ezreal', '82': 'Mordekaiser', '83': 'Yorick', '84': 'Akali', '85': 'Kennen', '86': 'Garen', '89': 'Leona', '90': 'Malzahar', '91': 'Talon', '92': 'Riven', '96': 'KogMaw', '98': 'Shen', '99': 'Lux', '101': 'Xerath', '102': 'Shyvana', '103': 'Ahri', '104': 'Graves', '105': 'Fizz', '106': 'Volibear', '107': 'Rengar', '110': 'Varus', '111': 'Nautilus', '112': 'Viktor', '113': 'Sejuani', '114': 'Fiora', '115': 'Ziggs', '117': 'Lulu', '119': 'Draven', '120': 'Hecarim', '121': 'Khazix', '122': 'Darius', '126': 'Jayce', '127': 'Lissandra', '131': 'Diana', '133': 'Quinn', '134': 'Syndra', '136': 'AurelionSol', '143': 'Zyra', '150': 'Gnar', '154': 'Zac', '157': 'Yasuo', '161': 'Velkoz', '163': 'Taliyah', '164': 'Camille', '201': 'Braum', '202': 'Jhin', '203': 'Kindred', '222': 'Jinx', '223': 'TahmKench', '236': 'Lucian', '238': 'Zed', '240': 'Kled', '245': 'Ekko', '254': 'Vi', '266': 'Aatrox', '267': 'Nami', '268': 'Azir', '412': 'Thresh', '420': 'Illaoi', '421': 'RekSai', '427': 'Ivern', '429': 'Kalista', '432': 'Bard', '497': 'Rakan', '498': 'Xayah', '516': 'ORNN', '517': 'SYLAS', '518': 'NEEKO', '523': 'APHELIOS', '555': 'PYKE', '235': 'SENNA', '875': 'SETT'
+	}
 
 
 	Object.freeze(Champions);
@@ -318,17 +318,17 @@ client.on("message", async message => {
 	if (message.content.startsWith("!m")) {
 
 
-	
-		
-	   
-		
+
+
+
+
 
 
 
 		let data;
 		for (i = 0; i < 10; i++) {
 
-		let args = message.content.substring(1).split(" ");
+			let args = message.content.substring(1).split(" ");
 			args.splice(0, 1);
 			const username = args.join(" ");
 
@@ -340,19 +340,19 @@ client.on("message", async message => {
 
 			var sum;
 			var regionID = "la2";
-		
 
-//build embed
+
+			//build embed
 			const embed = new Discord.RichEmbed()
-			.setTitle("Partida de "+username)
-			.setColor(0x00AE86)
-			.setDescription("Ranked normal que se yo.")
-			.setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-			.setThumbnail("https://vignette.wikia.nocookie.net/leagueoflegendsoficial/images/8/8c/LOL_Logo.png")
-			.setTimestamp()
+				.setTitle("Partida de " + username)
+				.setColor(0x00AE86)
+				.setDescription("Ranked normal que se yo.")
+				.setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+				.setThumbnail("https://vignette.wikia.nocookie.net/leagueoflegendsoficial/images/8/8c/LOL_Logo.png")
+				.setTimestamp()
 
-		//get summoner ID
-		
+			//get summoner ID
+
 			try {
 				sum = await pyke.summoner.getBySummonerName(String(username), regionID);
 
@@ -366,26 +366,26 @@ client.on("message", async message => {
 
 
 
-//get ranks
+			//get ranks
 
-try {
-	//console.log("SUMMMMM ID: "+sum.id);
-	 leaguePos = await pyke.league.getAllLeaguePositionsForSummoner(sum.id, regionID);
+			try {
+				//console.log("SUMMMMM ID: "+sum.id);
+				leaguePos = await pyke.league.getAllLeaguePositionsForSummoner(sum.id, regionID);
 
-	 tierSD = leaguePos.all.RANKED_FLEX_SR.tier;
-rankSD = leaguePos.all.RANKED_FLEX_SR.rank;
-lp = leaguePos.all.RANKED_FLEX_SR.leaguePoints;
-
-
-	 console.log(leaguePos);
-
-	//console.log(data);
-} catch (err) {
-	console.log(err);
+				tierSD = leaguePos.all.RANKED_FLEX_SR.tier;
+				rankSD = leaguePos.all.RANKED_FLEX_SR.rank;
+				lp = leaguePos.all.RANKED_FLEX_SR.leaguePoints;
 
 
+				console.log(leaguePos);
 
-}
+				//console.log(data);
+			} catch (err) {
+				console.log(err);
+
+
+
+			}
 
 
 			//get summoner name
@@ -396,7 +396,7 @@ lp = leaguePos.all.RANKED_FLEX_SR.leaguePoints;
 			} catch (err) {
 				console.log(err);
 				playerName = data.participants[i].summonerName;
-				
+
 
 
 			}
@@ -407,15 +407,15 @@ lp = leaguePos.all.RANKED_FLEX_SR.leaguePoints;
 
 
 
-embed.addField(data.participants[i].summonerName+" "+getChampionName(data.participants[i].championId), true).addBlankField(true)
-		.addField(tierSD + " " + rankSD + " " + lp + "PL", true);
+			embed.addField(data.participants[i].summonerName + " " + getChampionName(data.participants[i].championId), true).addBlankField(true)
+				.addField(tierSD + " " + rankSD + " " + lp + "PL", true);
 
-			console.log(data.participants[i].summonerName+"\n "+getChampionName(data.participants[i].championId)+data.all.RANKED_FLEX_SR.tier + " " + data.all.RANKED_FLEX_SR.rank + " " + data.all.RANKED_FLEX_SR.leaguePoints + "PL");
+			console.log(data.participants[i].summonerName + "\n " + getChampionName(data.participants[i].championId) + tierSD + " " + rankSD + " " + lp + "PL");
 		}
 
 
 
-		message.channel.send({embed});
+		message.channel.send({ embed });
 
 		//console.log(playerName +" "+ response);
 
