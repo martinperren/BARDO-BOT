2dcb24d8f07972b839a84e2b0e17036de317f8a7
Martin Perren <martinperren@gmail.com>
2021-07-02 20:33:18 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,42 +33,7 @@ function getLastVersion() {
 }
 */
 
-client.on("ready", () => {
-  console.log("Bot iniciado");
 
-  client.api.applications(client.user.id).commands.post({
-    data: {
-      name: "ping",
-      description: "ping pong!",
-    },
-  });
-
-  client.api.applications(client.user.id).commands.post({
-    data: {
-      name: "hello",
-      description: "hello world command",
-      // possible options here e.g. options: [{...}]
-    },
-  });
-
-  client.ws.on("INTERACTION_CREATE", async (interaction) => {
-    const command = interaction.data.name.toLowerCase();
-    const args = interaction.data.options;
-
-    if (command === "hello") {
-      // here you could do anything. in this sample
-      // i reply with an api interaction
-      client.api.interactions(interaction.id, interaction.token).callback.post({
-        data: {
-          type: 4,
-          data: {
-            content: "hello world!!!",
-          },
-        },
-      });
-    }
-  });
-});
 
 function Player(nick, champ, leaguePos, maestria) {
   this.nick = nick;
